958ee260d8a8ba85b66e777c1e5b68b539debeb1
Dominik <DudekDominik80@gmail.com>
2025-10-13 17:58:19 +0200

    dsfsd

--- Laboratoria/Lab1/zadania1.ml
+++ Laboratoria/Lab1/zadania1.ml
@@ -1,10 +1,8 @@
 (*zadanie 1*)
 let third (a,b,c) = c;;
-
 let test11 = third(1,2,"j");;
-let test12= third((1,4));;(*rzuci wyjatek*)
+(*let test12= third((1,4));; *)(*rzuci wyjatek*)
 let test13 = third(([], [], []));;
-
 let test14 = third (true, 4.2, "ala");;
 
 (* zadanie 3 *)
@@ -14,8 +12,9 @@ let rec sum list =
  else List.hd list +. sum(List.tl list);;
 
  let test31 = sum ([1.0;2.0;3.0;4.6;5.4]);;
- let test32 = sum([]);;
- let test33 = sum(["a"; "b"]);; (*rzuci wyjatek*)
+ 
+
+ (*let test33 = sum(["a"; "b"]);; *) (*rzuci wyjatek*)
  let test34 = sum [-1.5; 0.5; 1.0];;
 
  (*zadanie 4*)
@@ -29,6 +28,5 @@ let rec sum list =
   let test43 = lessThanNum( [1;1;1;1;1], 1);;
   let test44 = lessThanNum( [3;4;5;3], 5);;
   let test45 = lessThanNum( [3;4;5;3], 4);;
-
   let test46 = lessThanNum([-3; -2; -1], 0);;
 
--- Laboratoria/Lab1/zadania1.scala
+++ Laboratoria/Lab1/zadania1.scala
@@ -1,9 +1,10 @@
 object zadania1 extends App{
   //zadanie 2
-  def genList(num1: Int, num2:Int): List[Int] = {
+
+  def genList(num1: Int, num2:Int): List[Int] = 
     if num1 > num2 then Nil 
     else num1::genList(num1+1, num2)
-  }
+  
 
   val t21 = genList(2,9)
   val t22 = genList(0,0)
